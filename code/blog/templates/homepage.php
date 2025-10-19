c7d109ff6eec60cd69e7fd945f2cc928cb6dd641
André <111359909+A-thepuma@users.noreply.github.com>
2025-10-19 13:49:35 +0200

    Isolez l'affichage du traitement PHP

---
+++ code/blog/templates/homepage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <title>Le blog de l'AVBN</title>
+  <link href="style.css" rel="stylesheet" />
+</head>
+<body>
+  <h1>Le super blog de l'AVBN !</h1>
+  <p>Derniers billets du blog :</p>
+
+  <?php foreach ($posts as $post): ?>
+    <div class="news">
+      <h3>
+        <?php echo htmlspecialchars($post['title'], ENT_QUOTES, 'UTF-8'); ?>
+        <em>le <?php echo htmlspecialchars($post['frenchCreationDate'], ENT_QUOTES, 'UTF-8'); ?></em>
+      </h3>
+      <p>
+        <?php echo nl2br(htmlspecialchars($post['content'], ENT_QUOTES, 'UTF-8')); ?>
+        <br />
+        <em><a href="#">Commentaires</a></em>
+      </p>
+    </div>
+    <hr>
+  <?php endforeach; ?>
+
+</body>
+</html>
--- code/db.sql
+++ code/db.sql
@@ -1,20 +1,28 @@
--- Ce fichier sert à initialiser la base de données
+-- ⚠️ Efface la base si elle existe déjà
+DROP DATABASE IF EXISTS blog;
+
+-- Crée la base en UTF8 (pas utf8mb4)
+CREATE DATABASE IF NOT EXISTS blog
+  DEFAULT CHARACTER SET utf8
+  COLLATE utf8_general_ci;
+
+USE blog;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-CREATE TABLE `billets` (
-  `id` int(11) NOT NULL,
-  `titre` varchar(255) NOT NULL,
-  `contenu` text NOT NULL,
-  `date_creation` datetime NOT NULL
+-- Table billets
+CREATE TABLE IF NOT EXISTS `billets` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `titre` VARCHAR(255) NOT NULL,
+  `contenu` TEXT NOT NULL,
+  `date_creation` DATETIME NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `billets` (`id`, `titre`, `contenu`, `date_creation`) VALUES
 (1, 'Bienvenue sur le blog de l\'AVBN !', 'Je vous souhaite à toutes et à tous la bienvenue sur le blog qui parlera de... l\'Association de VolleyBall de Nuelly !', '2022-02-17 16:28:41'),
 (2, 'L\'AVBN à la conquête du monde !', 'C\'est officiel, le club a annoncé à la radio hier soir \"J\'ai l\'intention de conquérir le monde !\".\r\nIl a en outre précisé que le monde serait à sa botte en moins de temps qu\'il n\'en fallait pour dire \"Association de VolleyBall de Nuelly\". Pas dur, ceci dit entre nous...', '2022-02-17 16:28:42');
 
-ALTER TABLE `billets`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `billets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+-- Pour caler l'AUTO_INCREMENT après insertion
+ALTER TABLE `billets` AUTO_INCREMENT = 3;
